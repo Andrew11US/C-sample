1c76af5369117961e98a4092bbd33d794c9bc28f
Andrii Halabuda <losAngelesBoy11@gmail.com>
2018-02-19 21:06:41 +0100

    Initial Commit

---
+++ sample/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sample
+//
+//  Created by Agent X on 2/19/18.
+//  Copyright © 2018 Andrii Halabuda. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
