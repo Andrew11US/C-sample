e617a7f6a32258c7112d5210ef880fd902b31171
Andrii Halabuda <losAngelesBoy11@gmail.com>
2018-02-20 22:23:02 +0100

    Added file

--- sample/main.c
+++ sample/main.c
@@ -7,9 +7,84 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+// func declaration
+double circle_area(double x)
+{
+    return M_PI * x * x;
+}
+
+// 3 arguments
+double triangle_area(double a, double b, double c)
+{
+    double s = (a + b + c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
+int main()
+{
+    // select figure
+    printf("1. Circle\n2. Triangle\n");
+    int selected;
+    scanf("%i", &selected);
+    
+    switch (selected)
+    {
+        case 1:
+        {
+            printf("Type radius:\t");
+            double r;
+            
+            scanf("%lg", &r);
+            if (r >= 0)
+            {
+                printf("Circle area:\t%lg\n", circle_area(r));
+            }
+            else
+            {
+                printf("Input < 0");
+            }
+            
+            break;
+        }
+        case 2:
+        {
+            printf("Type 3 sides of triangle:\n");
+            double a, b, c;
+            
+            scanf("%lg%lg%lg", &a, &b, &c);
+            printf("Triangle area:\t%lg\n", triangle_area(a, b, c));
+            //double s = (a + b + c) / 2;
+            //printf("Pole trojkata:%lg\t", sqrt(s * (s - a) * (s - b) * (s - c)));
+            
+            break;
+        }
+        default:
+        {
+            printf("Wrong number!");
+        }
+            
+    }
+    /*
+     printf("Podaj promien kola:\t");
+     double r;
+     double area;
+     
+     // read double from keyboard
+     scanf("%lg", &r);
+     area = M_PI * r * r;
+     
+     if (r >= 0)
+     {
+     printf("Pole kola: %lg\n", area);
+     printf("Pole: %lg\n", circle_area(r));
+     }
+     else
+     {
+     printf("Input < 0");
+     }
+     */
+    
     return 0;
 }
